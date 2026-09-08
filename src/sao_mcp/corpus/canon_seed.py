86de72051cde82fa84d30556b8ca2ff2d07292cf
starmerx-luxuyan <luxuyan@starmerx.com>
2026-09-08 10:47:04 +0800

    Load Biceps Archelon field boss content

--- src/sao_mcp/corpus/canon_seed.py
+++ src/sao_mcp/corpus/canon_seed.py
@@ -5,6 +5,7 @@ from sao_mcp.corpus.core import Catalog
 from sao_mcp.corpus.floor2 import apply_floor2_martial_arts_corpus
 from sao_mcp.corpus.floor3 import apply_floor3_spider_corpus
 from sao_mcp.corpus.floor4 import apply_floor4_shipwright_corpus
+from sao_mcp.corpus.floor4_fieldboss import apply_floor4_fieldboss_corpus
 from sao_mcp.corpus.items import apply_aincrad_item_seed
 from sao_mcp.corpus.skills import apply_aincrad_skill_seed
 from sao_mcp.corpus.weapons import apply_aincrad_weapon_seed
@@ -51,6 +52,7 @@ def apply_canon_seed(catalog: Catalog) -> Catalog:
     apply_floor2_martial_arts_corpus(catalog)
     apply_floor3_spider_corpus(catalog)
     apply_floor4_shipwright_corpus(catalog)
+    apply_floor4_fieldboss_corpus(catalog)
 
     catalog.weapons.setdefault(
         "anneal_blade",
