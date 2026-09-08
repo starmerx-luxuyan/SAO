f7fe42d2b1ca49969c4e146ce8a34a1d1ed3a020
starmerx-luxuyan <luxuyan@starmerx.com>
2026-09-08 14:26:38 +0800

    Load Floor 6 Myia and Dungeon of Trials corpus

--- src/sao_mcp/corpus/canon_seed.py
+++ src/sao_mcp/corpus/canon_seed.py
@@ -11,6 +11,7 @@ from sao_mcp.corpus.floor5_flag import apply_floor5_flag_corpus
 from sao_mcp.corpus.floor5_shortcut import apply_floor5_shortcut_boss_corpus
 from sao_mcp.corpus.floor6_ambush import apply_floor6_ambush_corpus
 from sao_mcp.corpus.floor6_stachion import apply_floor6_stachion_corpus
+from sao_mcp.corpus.floor6_trials import apply_floor6_trials_corpus
 from sao_mcp.corpus.items import apply_aincrad_item_seed
 from sao_mcp.corpus.skills import apply_aincrad_skill_seed
 from sao_mcp.corpus.weapons import apply_aincrad_weapon_seed
@@ -63,6 +64,7 @@ def apply_canon_seed(catalog: Catalog) -> Catalog:
     apply_floor5_shortcut_boss_corpus(catalog)
     apply_floor6_stachion_corpus(catalog)
     apply_floor6_ambush_corpus(catalog)
+    apply_floor6_trials_corpus(catalog)
 
     catalog.weapons.setdefault(
         "anneal_blade",
