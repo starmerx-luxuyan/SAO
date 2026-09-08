a417c7dfce43f1a8b0a7c5badde35fdddfe17004
starmerx-luxuyan <luxuyan@starmerx.com>
2026-09-08 09:17:12 +0800

    Add scenario package for playable Aincrad content

---
+++ src/sao_mcp/scenarios/__init__.py
@@ -0,0 +1,5 @@
+"""Playable Aincrad scenario services built from ordinary runtime systems."""
+
+from sao_mcp.scenarios.floor22_witch import Floor22WitchScenario, install_floor22_witch_scenario
+
+__all__ = ["Floor22WitchScenario", "install_floor22_witch_scenario"]
