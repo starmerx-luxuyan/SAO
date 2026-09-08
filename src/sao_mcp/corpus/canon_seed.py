d41aa7fcdba3b06a011e354c9c5209f1d0568a4d
starmerx-luxuyan <luxuyan@starmerx.com>
2026-09-08 20:47:26 +0800

    Load Floor 7 Fallen Elf pursuit corpus

--- src/sao_mcp/corpus/canon_seed.py
+++ src/sao_mcp/corpus/canon_seed.py
@@ -21,6 +21,7 @@ from sao_mcp.corpus.floor7_elfwar import apply_floor7_elfwar_corpus
 from sao_mcp.corpus.floor7_intrigue import apply_floor7_intrigue_corpus
 from sao_mcp.corpus.floor7_monsters import apply_floor7_monster_corpus
 from sao_mcp.corpus.floor7_nirrnir import apply_floor7_nirrnir_corpus
+from sao_mcp.corpus.floor7_pursuit import apply_floor7_pursuit_corpus
 from sao_mcp.corpus.items import apply_aincrad_item_seed
 from sao_mcp.corpus.skills import apply_aincrad_skill_seed
 from sao_mcp.corpus.weapons import apply_aincrad_weapon_seed
@@ -83,6 +84,7 @@ def apply_canon_seed(catalog: Catalog) -> Catalog:
     apply_floor7_nirrnir_corpus(catalog)
     apply_floor7_intrigue_corpus(catalog)
     apply_floor7_elfwar_corpus(catalog)
+    apply_floor7_pursuit_corpus(catalog)
 
     catalog.weapons.setdefault(
         "anneal_blade",
