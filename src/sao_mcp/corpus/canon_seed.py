a174c7c4fd812fb6db585580cb717f938ca09c15
starmerx-luxuyan <luxuyan@starmerx.com>
2026-09-08 15:40:13 +0800

    Load Floor 6 southern trail corpus

--- src/sao_mcp/corpus/canon_seed.py
+++ src/sao_mcp/corpus/canon_seed.py
@@ -11,6 +11,7 @@ from sao_mcp.corpus.floor5_flag import apply_floor5_flag_corpus
 from sao_mcp.corpus.floor5_shortcut import apply_floor5_shortcut_boss_corpus
 from sao_mcp.corpus.floor6_ambush import apply_floor6_ambush_corpus
 from sao_mcp.corpus.floor6_finale import apply_floor6_finale_corpus
+from sao_mcp.corpus.floor6_south import apply_floor6_south_corpus
 from sao_mcp.corpus.floor6_stachion import apply_floor6_stachion_corpus
 from sao_mcp.corpus.floor6_trials import apply_floor6_trials_corpus
 from sao_mcp.corpus.items import apply_aincrad_item_seed
@@ -66,6 +67,7 @@ def apply_canon_seed(catalog: Catalog) -> Catalog:
     apply_floor6_stachion_corpus(catalog)
     apply_floor6_ambush_corpus(catalog)
     apply_floor6_trials_corpus(catalog)
+    apply_floor6_south_corpus(catalog)
     apply_floor6_finale_corpus(catalog)
 
     catalog.weapons.setdefault(
