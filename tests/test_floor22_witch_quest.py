c3492e7587f96d60a8584f47b584244ee4f742de
starmerx-luxuyan <luxuyan@starmerx.com>
2026-09-08 09:18:45 +0800

    Test Floor 22 as scenario content instead of a runtime subclass

--- tests/test_floor22_witch_quest.py
+++ tests/test_floor22_witch_quest.py
@@ -1,23 +1,25 @@
-from sao_mcp.runtime.floor22_runtime import Floor22QuestAincradRuntime
+from sao_mcp.runtime.housing_runtime import HousingAincradRuntime
+from sao_mcp.scenarios.floor22_witch import install_floor22_witch_scenario
 
 
 def test_floor22_witch_quest_completion_unlocks_forest_house_purchase():
-    runtime = Floor22QuestAincradRuntime(seed=7)
+    runtime = HousingAincradRuntime(seed=7)
+    scenario = install_floor22_witch_scenario(runtime)
     player = runtime.create_character("Floor22", level=30)
     runtime.world.floors[22].unlocked = True
     player.location_id = "floor_22_forest_house_site"
     player.col = 6_000_000
 
-    instance = runtime.start_witch_quest([player.actor_id])
-    encounter = runtime.enter_witch_castle(instance["instance_id"])
+    instance = scenario.start([player.actor_id])
+    encounter = scenario.enter_castle(instance["instance_id"])
 
-    for panther_id in list(runtime._instance(instance["instance_id"])["werepanther_ids"]):
+    for panther_id in list(scenario.instance(instance["instance_id"])["werepanther_ids"]):
         panther = runtime.actors[panther_id]
         panther.hp = 0
         panther.alive = False
         runtime._resolve_defeat(encounter, panther, player.actor_id)
 
-    witch_encounter, witch = runtime.start_witch_confrontation(
+    witch_encounter, witch = scenario.start_confrontation(
         instance["instance_id"],
         accept_soup=True,
     )
@@ -25,7 +27,7 @@ def test_floor22_witch_quest_completion_unlocks_forest_house_purchase():
     witch.alive = False
     runtime._resolve_defeat(witch_encounter, witch, player.actor_id)
 
-    result = runtime.return_from_witch_quest(instance["instance_id"])
+    result = scenario.finish_return(instance["instance_id"])
     assert player.actor_id in result["completedPlayerIds"]
     assert "witch_of_the_west_and_three_treasures" in runtime.quests.completed_by_actor[player.actor_id]
     assert player.location_id == "floor_22_forest_house_site"
